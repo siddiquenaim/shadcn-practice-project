bb8f7388142c4aa72bd00a2b76d7084f12fff055
siddiquenaim <naim.niter.tex06@gmail.com>
2024-01-08 22:15:50 +0600

    Completed sorting example

--- app/people/PeopleDataTable.tsx
+++ app/people/PeopleDataTable.tsx
@@ -2,9 +2,12 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
+  SortingState,
   flexRender,
   getCoreRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -17,7 +20,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -28,51 +32,108 @@ export function PeopleDataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  //local states for table functionality
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  // setting table data
   const table = useReactTable({
+    //fetching data
     data,
     columns,
+
+    //loading functions
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+
+    //on action
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+
+    //updating state
+    state: {
+      sorting,
+      columnFilters,
+    },
   });
 
   return (
-    <div className="rounded-md border mb-20">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-
-        <TableBody>
-          {table.getRowModel().rows.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div className="mb-20">
+      {/* table */}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell>No Result</TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell>No Result</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* pagination */}
+      <div className="flex justify-end space-x-2 my-2">
+        <Button
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+          variant="outline"
+        >
+          {`<<`}
+        </Button>
+        <Button
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+          variant="outline"
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+          variant="outline"
+        >
+          Next
+        </Button>
+        <Button
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+          variant="outline"
+        >
+          {`>>`}
+        </Button>
+      </div>
     </div>
   );
 }
--- app/people/column.tsx
+++ app/people/column.tsx
@@ -3,6 +3,8 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Person } from "../data/people";
 import DropDownCopy from "@/components/custom/DropDownCopy";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
 
 export const columns: ColumnDef<Person>[] = [
   {
@@ -23,7 +25,18 @@ export const columns: ColumnDef<Person>[] = [
   },
   {
     accessorKey: "date_of_birth",
-    header: "Birth Date",
+    header: ({ header }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() =>
+            header.column.toggleSorting(header.column.getIsSorted() === "asc")
+          }
+        >
+          Date of Birth <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       const date_of_birth = row.getValue("date_of_birth");
       const formatted = new Date(date_of_birth as string).toLocaleDateString();
