52684d1a75aae46129d19f287a4eb0028658cc89
siddiquenaim <naim.niter.tex06@gmail.com>
2024-01-11 22:04:17 +0600

    completed zundand simple example

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import NavBar from "@/components/custom/Nav";
+"use client";
+import { useCounterStore } from "./store";
 
 export default function Home() {
-  return <main></main>;
+  const count = useCounterStore((state) => state.count);
+  const incrementAsync = useCounterStore((state) => state.incrementAsync);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  return (
+    <main className="container text-center">
+      <div className="text-2xl">{count}</div>
+      <div className="space-x-2">
+        <button onClick={() => incrementAsync()}>IncrementAsync</button>
+        <button onClick={() => decrement()}>Decrement</button>
+      </div>
+    </main>
+  );
 }
