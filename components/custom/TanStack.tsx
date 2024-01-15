e1048bb4f8c0977db0680d733da9e4ebf325af9f
siddiquenaim <naim.niter.tex06@gmail.com>
2024-01-15 14:33:30 +0600

    Basic use of react query

--- components/custom/TanStack.tsx
+++ components/custom/TanStack.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import wait from "./wait";
+import { Button } from "../ui/button";
 
 const Posts: {
   id: number;
@@ -17,12 +20,50 @@ const Posts: {
 ];
 
 const TanStack = () => {
+  const queryClient = useQueryClient();
+
   const postQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...Posts]),
   });
 
-  return <div className="text-2xl text-white">TanStack Query</div>;
+  const newPostMutation = useMutation({
+    mutationFn: async (title: string) => {
+      return await wait(1000).then(() =>
+        Posts.push({ id: crypto.randomUUID, title })
+      );
+    },
+    onSuccess: () => {
+      postQuery.refetch();
+      // queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  if (postQuery.isLoading) {
+    return <div>Loading..</div>;
+  }
+
+  if (postQuery.error) {
+    return <pre>{JSON.stringify(postQuery.error)}</pre>;
+  }
+
+  return (
+    <div className="text-white">
+      <h1 className="text-2xl my-10">TanStack Query</h1>
+      <div>
+        {postQuery.data?.map((singlePost) => (
+          <p key={singlePost.id}>{singlePost.title}</p>
+        ))}
+      </div>
+      <Button
+        className="btn btn-primary mt-3"
+        onClick={() => newPostMutation.mutate("New Post")}
+        variant="outline"
+      >
+        Add New
+      </Button>
+    </div>
+  );
 };
 
 export default TanStack;
